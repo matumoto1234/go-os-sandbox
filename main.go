cc68a34ab1ae690d835c6d6a688453af95c4f0ae
matumoto1234 <matumotosizuoka@gmail.com>
2022-05-04 18:34:52 +0900

    nice add

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+func git(arg string) error {
+	err := exec.Command("git", arg).Run()
+	if err != nil {
+		return errors.New(fmt.Sprintf("git: run the command [git %v]\n", arg))
+	}
+	return nil
+}
+
+func main() {
+	err := os.MkdirAll("hoge/fuga/piyo", 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	content := "hogehoge\nfugafuga\npiyopiyooooo!!!!"
+	ioutil.WriteFile("hoge/a.txt", []byte(content), 0755)
+
+	ioutil.WriteFile("hoge/fuga/piyo/b.txt", []byte(content), 0755)
+
+	err = exec.Command("git", "add", ".").Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
